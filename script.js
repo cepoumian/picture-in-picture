04f16e0956f7d3d499adab7bb00e1254f6e8f4f3
Cesar Poumian <cepoumian@Cesars-MacBook-Pro.local>
2020-10-01 22:14:52 -0600

    Bypass the video object and have the start button directly launch the picture in picture functionality. The function on the event listener of the start button should be asynchronous; it should disable the button, await for the picture in picture promise and only if it is successful, enable the button again.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-// Prompt the user to select a media stream; pass that to video element and play.
+// Prompt the user to select a media stream; pass that to the video element as its source object; the video plays after it hasfinished loading its metadata.
 async function selectMediaStream() {
   try {
-      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+      }
   } catch (error) {
     console.log('Error caught: ', error);
   }
 }
+
+button.addEventListener('click', async () => {
+  // Disable button
+  button.disabled = true;
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+  // Reset button
+  button.disabled = false;
+});
 
 // On load
 selectMediaStream();
